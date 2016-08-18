0bca01651ff14e369d5ffb1101cbdf30484581fc
Ryan <dragonguard7@gmail.com>
2016-08-18 17:57:51 -0400

    Added recursive program that counts the number of times hi appears in a string

---
+++ java/countHi.java
@@ -0,0 +1,16 @@
+/*
+Given a string, compute recursively (no loops) the number of times lowercase "hi" appears in the string.
+
+countHi("xxhixx") -> 1
+countHi("xhixhix") -> 2
+countHi("hi") -> 1
+*/
+public int countHi(String str) {
+  if(str.length() < 2){
+    return 0;
+  }
+  if(str.substring(0,2).equals("hi")){
+    return 1 + countHi(str.substring(2,str.length()));
+  }
+  return countHi(str.substring(1,str.length()));
+}
